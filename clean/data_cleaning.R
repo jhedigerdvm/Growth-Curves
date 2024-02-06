4c9e7276cee4cd71a1a3987b6259e0cc2528f79c
Joe.Hediger <joseph.hediger@students.tamuk.edu>
2024-02-06 15:45:14 -0600

    data clean

--- clean/data_cleaning.R
+++ clean/data_cleaning.R
@@ -55,9 +55,16 @@ write.csv(data, './clean/morpho_rain.csv', row.names = F)
 #######333
 data <- read.csv('./raw/master_data.csv', header = T)
 data1 <- data %>%  filter(Age != '0.5') #contains 2022 data
-data2 <- data1[,c(1:4, 8,9,15:19, 22,28:44)]
+data2 <- data1[,c(1:4, 8,9,15:19, 22,28:42)]
 data3 <- data2
 names(data3) <- c('animal_id', 'date', 'year_cap', 'year_birth', 'age', 'weight_lb', 'bs', 'points_typ', 
                   'points_total', 'mass', 'bcs_in', 'spike', 'gray_face', 'roman_nose', 'wrinkled_ears', 
                   'loose_face', 'loose_neck', 'loose_chest', 'circ_face_eyes', 'circ_nosebridge', 'eyes_nose',
-                  )
+                  'ear_ear', 'stomach', 'neck', 'tarsal_height', 'tarsal_width', 'stain')
+data3[data3 == ""] <- NA
+
+#need to update birth year based upon animal-id
+for (i in nrow:data3){
+  
+  
+}
